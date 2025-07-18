d158664490fc0428ae6154170d0424b6eb318adc
Ayushi Sahu <ayushisahu222@gmail.com>
2025-07-18 21:00:50 +0530

    Update agent.py

--- google-adk-agents/root_agent/sub_agents/time_agent/agent.py
+++ google-adk-agents/root_agent/sub_agents/time_agent/agent.py
@@ -11,11 +11,11 @@ from .prompt import TIME_AGENT_PROMPT
 from .tools import current_time
 
 # Constants
-GPT_MODEL = os.getenv("GPT_MODEL", LiteLlm(model="openai/gpt-4o"))
+GPT_MODEL = os.getenv("GPT_MODEL")
 # GEMINI_MODEL = os.getenv("GEMINI_MODEL","gemini-2.0-flash-001")
 
 get_time_agent = LlmAgent(
-    model=GPT_MODEL,
+    model=LiteLlm(model=GPT_MODEL), #Replace LiteLlm(model=GPT_MODEL) with GEMINI_MODEL if using Gemini
     name="Get_Time_Agent",
     description="Gives user current time.",
     instruction=TIME_AGENT_PROMPT,
